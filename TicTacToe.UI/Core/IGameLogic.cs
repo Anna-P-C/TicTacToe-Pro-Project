ce9c62eadfc178ab9e84173074417f58e5879b9c
Anna Polyakova <ipz242_pas@student.ztu.edu.ua>
2026-04-18 20:50:59 +0300

    feat: implement game engine, interfaces and player model

---
+++ TicTacToe.UI/Core/IGameLogic.cs
@@ -0,0 +1,10 @@
+﻿namespace TicTacToe.UI.Core
+{
+    public interface IGameLogic
+    {
+        bool MakeMove(int row, int col, char symbol);
+        char CheckWinner();
+        void ResetBoard();
+        char[,] GetBoardState();
+    }
+}
---
+++ TicTacToe.UI/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿namespace TicTacToe.UI.Models
+{
+    public class Player
+    {
+        public string Name { get; set; }
+        public char Symbol { get; set; }
+        public int Score { get; set; }
+
+        public Player(string name, char symbol)
+        {
+            Name = name;
+            Symbol = symbol;
+            Score = 0;
+        }
+    }
+}
