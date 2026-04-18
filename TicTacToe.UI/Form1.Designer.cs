7e303f086f1c9a87c25961168995ab0ae8d00143
Anna Polyakova <ipz242_pas@student.ztu.edu.ua>
2026-04-18 21:33:49 +0300

    feat: finalize game logic and UI connection, fix project file structure

--- TicTacToe.UI/Form1.Designer.cs
+++ TicTacToe.UI/Form1.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace TicTacToe.UI
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace TicTacToe.UI
 {
     partial class Form1
     {
@@ -28,12 +31,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            btn00 = new Button();
+            btn01 = new Button();
+            btn02 = new Button();
+            btn10 = new Button();
+            btn11 = new Button();
+            btn20 = new Button();
+            btn21 = new Button();
+            btn22 = new Button();
+            btn12 = new Button();
+            SuspendLayout();
+            // 
+            // btn00
+            // 
+            btn00.Location = new Point(61, 43);
+            btn00.Name = "btn00";
+            btn00.Size = new Size(75, 75);
+            btn00.TabIndex = 0;
+            btn00.UseVisualStyleBackColor = true;
+            btn00.Click += OnButtonClick;
+            // 
+            // btn01
+            // 
+            btn01.Location = new Point(307, 43);
+            btn01.Name = "btn01";
+            btn01.Size = new Size(75, 75);
+            btn01.TabIndex = 1;
+            btn01.UseVisualStyleBackColor = true;
+            btn01.Click += OnButtonClick;
+            // 
+            // btn02
+            // 
+            btn02.Location = new Point(540, 43);
+            btn02.Name = "btn02";
+            btn02.Size = new Size(75, 75);
+            btn02.TabIndex = 2;
+            btn02.UseVisualStyleBackColor = true;
+            btn02.Click += OnButtonClick;
+            // 
+            // btn10
+            // 
+            btn10.Location = new Point(61, 199);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(75, 75);
+            btn10.TabIndex = 3;
+            btn10.UseVisualStyleBackColor = true;
+            btn10.Click += OnButtonClick;
+            // 
+            // btn11
+            // 
+            btn11.Location = new Point(307, 199);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(75, 75);
+            btn11.TabIndex = 4;
+            btn11.UseVisualStyleBackColor = true;
+            btn11.Click += OnButtonClick;
+            // 
+            // btn20
+            // 
+            btn20.Location = new Point(61, 375);
+            btn20.Name = "btn20";
+            btn20.Size = new Size(75, 75);
+            btn20.TabIndex = 5;
+            btn20.UseVisualStyleBackColor = true;
+            btn20.Click += OnButtonClick;
+            // 
+            // btn21
+            // 
+            btn21.Location = new Point(307, 375);
+            btn21.Name = "btn21";
+            btn21.Size = new Size(75, 75);
+            btn21.TabIndex = 6;
+            btn21.UseVisualStyleBackColor = true;
+            btn21.Click += OnButtonClick;
+            // 
+            // btn22
+            // 
+            btn22.Location = new Point(540, 375);
+            btn22.Name = "btn22";
+            btn22.Size = new Size(75, 75);
+            btn22.TabIndex = 7;
+            btn22.UseVisualStyleBackColor = true;
+            btn22.Click += OnButtonClick;
+            // 
+            // btn12
+            // 
+            btn12.Location = new Point(540, 199);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(75, 75);
+            btn12.TabIndex = 8;
+            btn12.UseVisualStyleBackColor = true;
+            btn12.Click += OnButtonClick;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(707, 532);
+            Controls.Add(btn12);
+            Controls.Add(btn22);
+            Controls.Add(btn21);
+            Controls.Add(btn20);
+            Controls.Add(btn11);
+            Controls.Add(btn10);
+            Controls.Add(btn02);
+            Controls.Add(btn01);
+            Controls.Add(btn00);
+            Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button btn00;
+        private Button btn01;
+        private Button btn02;
+        private Button btn10;
+        private Button btn11;
+        private Button btn20;
+        private Button btn21;
+        private Button btn22;
+        private Button btn12;
     }
 }
--- TicTacToe.UI/Form1.cs
+++ TicTacToe.UI/Form1.cs
@@ -1,10 +1,54 @@
+using TicTacToe.UI.Core;
+using TicTacToe.UI.Models;
+using TicTacToe.UI.Services;
+using System;
+using System.Windows.Forms;
+
 namespace TicTacToe.UI
 {
     public partial class Form1 : Form
     {
+        private readonly IGameLogic _gameEngine;
+        private Player _player1;
+        private Player _player2;
+        private Player _currentPlayer;
         public Form1()
         {
             InitializeComponent();
+
+            _gameEngine = new GameEngine();
+            _player1 = new Player("ֳנאגוצü 1", 'X');
+            _player2 = new Player("ֳנאגוצü 2", 'O');
+            _currentPlayer = _player1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnButtonClick(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int row = int.Parse(button.Name.Substring(3, 1));
+            int col = int.Parse(button.Name.Substring(4, 1));
+
+            if (_gameEngine.MakeMove(row, col, _currentPlayer.Symbol))
+            {
+                button.Text = _currentPlayer.Symbol.ToString();
+                button.Enabled = false; 
+
+                char winner = _gameEngine.CheckWinner();
+                if (winner != '\0')
+                {
+                    MessageBox.Show($"ֿונול³ד דנאגוצü: {winner}!");
+                    Application.Restart();
+                }
+                else
+                {
+                    _currentPlayer = (_currentPlayer == _player1) ? _player2 : _player1;
+                }
+            }
         }
     }
 }
