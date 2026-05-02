9092134ab37dd7da43bd50a77062627fdbba7f53
Anna Polyakova <ipz242_pas@student.ztu.edu.ua>
2026-05-02 14:23:50 +0300

    test

--- TicTacToe.Tests/GameLogicTests.cs
+++ TicTacToe.Tests/GameLogicTests.cs
@@ -1,83 +1,48 @@
 ﻿using Xunit;
 using TicTacToe.UI.Services;
-using TicTacToe.UI.Core;
-
+using TicTacToe.UI.Models; 
 namespace TicTacToe.Tests
 {
     public class GameLogicTests
     {
-        private readonly GameEngine _engine;
-
-        public GameLogicTests()
-        {
-            _engine = new GameEngine();
-        }
-
-        [Fact]
-        public void Board_ShouldBeEmpty_OnStart()
-        {
-            var board = _engine.GetBoard();
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                {
-                    Assert.Equal('\0', board[i, j]);
-                }
-            }
-        }
-
-        [Fact]
-        public void MakeMove_ShouldSetSymbol_WhenCellIsEmpty()
-        {
-            bool success = _engine.MakeMove(0, 0, 'X');
-            var board = _engine.GetBoard();
-
-            Assert.True(success);
-            Assert.Equal('X', board[0, 0]);
-        }
-
-        [Fact]
-        public void MakeMove_ShouldReturnFalse_WhenCellIsOccupied()
-        {
-            _engine.MakeMove(1, 1, 'X');
-            bool success = _engine.MakeMove(1, 1, 'O');
-
-            Assert.False(success);
-        }
-
         [Theory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(2)]
-        public void CheckWinner_ShouldDetectHorizontalWin(int row)
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void InitializeBoard_CorrectSizeCreated(int size)
         {
-            _engine.ResetBoard();
-            _engine.MakeMove(row, 0, 'X');
-            _engine.MakeMove(row, 1, 'X');
-            _engine.MakeMove(row, 2, 'X');
+            var engine = new GameEngine();
+            engine.InitializeNewBoard(size);
+            var board = engine.GetBoard();
 
-            Assert.Equal('X', _engine.CheckWinner());
+            Assert.Equal(size, board.GetLength(0));
+            Assert.Equal(size, board.GetLength(1));
         }
 
         [Fact]
-        public void TournamentManager_ShouldProgress_AfterWin()
+        public void CheckWinner_5x5_DiagonalWin_ReturnsX()
+        {
+            var engine = new GameEngine();
+            engine.InitializeNewBoard(5);
+
+            
+            for (int i = 0; i < 5; i++)
+                engine.MakeMove(i, i, 'X');
+
+            Assert.Equal('X', engine.CheckWinner());
+        }
+
+        [Fact]
+        public void TournamentManager_ScoresUpdateCorrectly()
         {
             var manager = new TournamentManager();
-            int initialRound = manager.CurrentRound;
 
+          
             manager.RegisterWin('X');
-
-            Assert.Equal(initialRound + 1, manager.CurrentRound);
             Assert.Equal(100, manager.TotalScore);
-        }
 
-        [Fact]
-        public void Tournament_ShouldEnd_WhenBotWins()
-        {
-            var manager = new TournamentManager();
-            manager.RegisterWin('O');
-
-            Assert.False(manager.IsTournamentActive);
+            
+            Assert.Equal(2, manager.CurrentRound);
         }
     }
 }
---
+++ TicTacToe.Tests/WinConditionTests.cs
@@ -0,0 +1,38 @@
+﻿using Xunit;
+using TicTacToe.UI.Services;
+
+namespace TicTacToe.Tests
+{
+    public class WinConditionTests
+    {
+        
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void CheckWinner_5x5_HorizontalWin_ReturnsX(int row)
+        {
+            var engine = new GameEngine();
+            engine.InitializeNewBoard(5);
+            for (int col = 0; col < 5; col++) engine.MakeMove(row, col, 'X');
+            Assert.Equal('X', engine.CheckWinner());
+        }
+
+        // Перевірка вертикалей на 5х5
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        public void CheckWinner_5x5_VerticalWin_ReturnsO(int col)
+        {
+            var engine = new GameEngine();
+            engine.InitializeNewBoard(5);
+            for (int row = 0; row < 5; row++) engine.MakeMove(row, col, 'O');
+            Assert.Equal('O', engine.CheckWinner());
+        }
+    }
+}
